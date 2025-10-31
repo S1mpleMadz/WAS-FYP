fe60f160564eb20efda0aa9cc779111fa1c53d8e
S1mpleMadz <113286173+S1mpleMadz@users.noreply.github.com>
2025-10-31 18:16:24 +0000

    Sprint 1 (2nd video)
    Setting up getting data from the api
    
    and also ensuring the folder format is followed so (info.txt) files have been added for github to copy the folders over

--- src/components/pages/Staff.js
+++ src/components/pages/Staff.js
@@ -1,14 +1,39 @@
-function Staff() {
-  // Properties
+import { useState } from "react";
 
-  // Hooks
+function Staff() {
+  // Initialisation
+
+  const loggedinUser = null;
+  const endpoint = "/users";
+
+  // State
+  const [users, setUsers] = useState(null);
+  const [loadingMessage, setLoadingMessage] = useState("Loading records...");
 
   // Context
 
   // Methods
+  const URL = "http://localhost:5000/api";
+  const endpointaddress = URL + endpoint;
+  fetch(endpointaddress);
 
   // View
-  return <h1>Welome to Staff</h1>;
+  return (
+    <section>
+      <h1>Welome to Staff</h1>
+      {!users ? (
+        <p>{loadingMessage}</p>
+      ) : users.length === 0 ? (
+        <p>No users found</p>
+      ) : (
+        users.map((user) => (
+          <p>
+            {user.UserFirstname} {user.UserLastname}
+          </p>
+        ))
+      )}
+    </section>
+  );
 }
 
 export default Staff;
